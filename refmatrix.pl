83f6bd5ed7ba630c229f5791327b68cc4bc32336
ival0 <46320091+ival00@users.noreply.github.com>
2019-03-24 19:55:26 +0300

    Update refmatrix.pl

--- refmatrix.pl
+++ refmatrix.pl
@@ -9,6 +9,7 @@ my $j;
 my $Bufer;
 my $file_name = "matrix.dat";
 my @matrix;
+my $ref_matrix=\@matrix; 
 unless ( defined $file_name ) {
     die "No argument!";
 }
@@ -27,27 +28,21 @@ exit(0) if -z $file_name;
 
 open( my $fh, "$file_name" ) or die "Can't open '$file_name'!\n";
 $matrix_range=0;
-print "file opened\n";
 while (!eof){
-  my $line=$fh;
+  my $line=readline($fh);
   chomp($line);
-  print "$line\n";
-  @matrix[$matrix_range]=split( '\t', $line);
+  @matrix[$matrix_range]=split( /\t/, $line);
   $matrix_range++;
 }
+$matrix_range--;
 close $fh;
-print "file closed";
-foreach $i (@matrix){
-  print "$i\n";
+for $i (0..$matrix_range){
+  for $j (0..$matrix_range){
+    print "$matrix[$i,$j]\n";
+  }
 }
-#@matrix=[
-#      [1, 2, 3, 4, 5],
-#      [6, 7, 8, 9, 10],
-#      [11, 12, 13, 14, 15],
-#      [16, 17, 18, 19, 20],
-#      [21, 22, 23, 24, 25]
-#      ];
-my $ref_matrix=\@matrix;  
+
+ 
    
 for $i(0..$matrix_range-1) {
   for $j($i+1..$matrix_range){

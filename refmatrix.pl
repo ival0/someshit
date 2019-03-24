f520baba1bb004148c1b937cd2cbde482549d69b
ival0 <46320091+ival00@users.noreply.github.com>
2019-03-24 15:00:06 +0300

    Update refmatrix.pl

--- refmatrix.pl
+++ refmatrix.pl
@@ -6,13 +6,14 @@ use warnings;
 my $i;
 my $j;
 my $Bufer;
-my $ref_matrix=[
+my @matrix=[
       [1, 2, 3, 4, 5],
       [6, 7, 8, 9, 10],
       [11, 12, 13, 14, 15],
       [16, 17, 18, 19, 20],
       [21, 22, 23, 24, 25]
       ];
+my $ref_matrix=\@matrix;      
 for $i(0..3) {
   for $j($i+1..4){
     $Bufer=${$ref_matrix}->[$i][$j];

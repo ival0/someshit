94e9df167d6799fc999cf44ff556d332812c6849
ival0 <46320091+ival00@users.noreply.github.com>
2019-03-27 21:38:09 +0300

    Update refmatrix.pl

--- refmatrix.pl
+++ refmatrix.pl
@@ -3,57 +3,48 @@
 use strict;
 use warnings;
 
-my $matrix_range;
 my $i;
 my $j;
 my $Bufer;
 my $file_name = "matrix.dat";
 my @matrix;
-my $ref_matrix=\@matrix; 
+my $ref_matrix=\@matrix;
+my $rows;
+my $columns;
+sub trasperense(my $rows, my $columns, my $refs){
+
+}
 unless ( defined $file_name ) {
     die "No argument!";
 }
-
 unless ( -e $file_name ) {
 
   die "No such file: '$file_name'!";
 }
-
 unless ( -r $file_name ) {
 
   die "No read access to file: '$file_name'!";
 }
-
 exit(0) if -z $file_name;
 
 open( my $fh, "$file_name" ) or die "Can't open '$file_name'!\n";
-$matrix_range=0;
 while (!eof){
   my $line=readline($fh);
   chomp($line);
-  push @matrix, [split( /\t/, $line)];
-  $matrix_range++;
+  push (@{$ref_matrix}, [split( /\t/, $line)]);
 }
-$matrix_range--;
 close $fh;
-for $i (0..$matrix_range){
-  for $j (0..$matrix_range){
-    print "$ref_matrix->[$i][$j]"."|";
-  }
-  print "\n";
-}
-print "was 2 for\n";
    
-for $i(0..$matrix_range-1) {
-  for $j($i+1..$matrix_range){
+for $i(0..$#{$ref_matrix}-1) {
+  for $j($i+1..$#{$ref_matrix}){
     $Bufer=$ref_matrix->[$i][$j];
     $ref_matrix->[$i][$j]=$ref_matrix->[$j][$i];
     $ref_matrix->[$j][$i]=$Bufer;
   }
 }
-for $i(0..$matrix_range) {
-  for $j(0..$matrix_range) {
-    print "$ref_matrix->[$i][$j]"."|";
+for $i(0..$#{$ref_matrix}) {
+  for $j(0..$#{$ref_matrix}) {
+    print "$ref_matrix->[$i][$j]"." ";
   }
   print "\n";
 }

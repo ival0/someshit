16e97ce45670c549110bc867bb6158c1133c2cb5
ival0 <46320091+ival00@users.noreply.github.com>
2019-03-30 19:24:36 +0300

    1

--- refmatrix.pl
+++ refmatrix.pl
@@ -7,9 +7,9 @@ my $i;
 my $j;
 my $Bufer;
 my $file_name = "matrix.dat";
-my @matrix;
+my @matrix=[];
 my $ref_matrix=\@matrix;
-my @Zero_row;
+my @Zero_row=[];
 my $ref_zero_row=\@Zero_row;
 my $rows;
 my $columns;
@@ -31,30 +31,50 @@ exit(0) if -z $file_name;
 
 open( my $fh, "$file_name" ) or die "Can't open '$file_name'!\n";
 while (!eof){
-  my $line=readline($fh);
+  my $line=<$fh>;
   chomp($line);
   push (@{$ref_matrix}, [split( /\t/, $line)]);
 }
 close $fh;
+
 $rows=$#{$ref_matrix};
 $columns=$#{$ref_matrix->[1]};
+shift @{$ref_matrix};
 
-
+#print "matrix input\n";
+#foreach $i(@{$ref_matrix}) {
+#  foreach $j(@{$i}) {
+#    print "$j"."|";
+#  }
+#  print "\n";
+#}
+#print "end matrix\n";
 if ($rows>$columns){
   for $i (0..$rows){
-    for $j ($columns..$rows){
+    $j=$columns;
+    while($j++<$rows){
     push (@{$ref_matrix->[$i]}, 0);
     }
   }
 } elsif ($columns>$rows){
-    ${ref_zero_row}=[];
     for $i (1..$columns){
       push(@{$ref_zero_row}, ['0']);  
     }
-    for $i ($rows..$columns){
+#    shift @{$ref_zero_row};
+    $i=$rows;
+    while ($i++<$columns){
       push @{$ref_matrix}, @{$ref_zero_row};
     }
   }
+ 
+#print "normalized matrix\n";
+#foreach $i(@{$ref_matrix}) {
+#  foreach $j(@{$i}) {
+#    print "$j"."|";
+#  }
+#  print "\n";
+#}
+#print "end matrix\n";
 for $i(0..$#{$ref_matrix}-1) {
   for $j($i+1..$#{$ref_matrix}){
     $Bufer=$ref_matrix->[$i][$j];
@@ -62,27 +82,29 @@ for $i(0..$#{$ref_matrix}-1) {
     $ref_matrix->[$j][$i]=$Bufer;
   }
 }
+
 $Bufer=$rows;
 $rows=$columns;
 $columns=$Bufer;
 
 if ($rows>$columns){
   for $i (0..$rows){
-    for $j ($columns..$rows){
-    pop @{$ref_matrix->[$i]};
+    $j=$columns;
+    while ($j++<$rows){
+      pop @{$ref_matrix->[$i]};
     }
   }
 
 } elsif ($columns>$rows){
-    for $i ($rows..$columns){
+    $i=$rows;
+    while ($i++<$columns){
       pop @{$ref_matrix};
     }
   }
 
-for $i(0..$rows) {
-  for $j(0..$columns) {
-    print "$ref_matrix->[$i][$j]"." ";
+foreach $i(@{$ref_matrix}) {
+  foreach $j(@{$i}) {
+    print "$j"." ";
   }
   print "\n";
 }
-

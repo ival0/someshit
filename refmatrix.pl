bdf298093074bf9b9400b3e112ad1641a3110c3e
ival0 <46320091+ival00@users.noreply.github.com>
2019-03-31 13:25:57 +0300

    1

--- refmatrix.pl
+++ refmatrix.pl
@@ -5,17 +5,15 @@ use warnings;
 
 my $i;
 my $j;
-my $Bufer;
+my $bufer;
 my $file_name = "matrix.dat";
-my @matrix=[];
-my $ref_matrix=\@matrix;
-my @Zero_row=[];
-my $ref_zero_row=\@Zero_row;
+my @matrix = [];
+my $ref_matrix = \@matrix;
+my @zero_row = [];
+my $ref_zero_row = \@zero_row;
 my $rows;
 my $columns;
-#sub trasperense(my $rows, my $columns, my $refs){
 
-#}
 unless ( defined $file_name ) {
     die "No argument!";
 }
@@ -31,28 +29,19 @@ exit(0) if -z $file_name;
 
 open( my $fh, "$file_name" ) or die "Can't open '$file_name'!\n";
 while (!eof){
-  my $line=<$fh>;
+  my $line = <$fh>;
   chomp($line);
   push (@{$ref_matrix}, [split( /\t/, $line)]);
 }
 close $fh;
 
-$rows=$#{$ref_matrix};
-$columns=$#{$ref_matrix->[1]};
+$rows = $#{$ref_matrix};
+$columns = $#{$ref_matrix->[1]};
 shift @{$ref_matrix};
 
-#print "matrix input\n";
-#foreach $i(@{$ref_matrix}) {
-#  foreach $j(@{$i}) {
-#    print "$j"."|";
-#  }
-#  print "\n";
-#}
-#print "end matrix\n";
 if ($rows>$columns){
   for $i (0..$rows){
-    $j=$columns;
-    while($j++<$rows){
+    while($#{$ref_matrix->[$i]}<$rows){
     push (@{$ref_matrix->[$i]}, 0);
     }
   }
@@ -60,32 +49,22 @@ if ($rows>$columns){
     for $i (1..$columns){
       push(@{$ref_zero_row}, ['0']);  
     }
-#    shift @{$ref_zero_row};
-    $i=$rows;
-    while ($i++<$columns){
+    while ($#{$ref_matrix}<$columns){
       push @{$ref_matrix}, @{$ref_zero_row};
     }
   }
- 
-#print "normalized matrix\n";
-#foreach $i(@{$ref_matrix}) {
-#  foreach $j(@{$i}) {
-#    print "$j"."|";
-#  }
-#  print "\n";
-#}
-#print "end matrix\n";
+
 for $i(0..$#{$ref_matrix}-1) {
   for $j($i+1..$#{$ref_matrix}){
-    $Bufer=$ref_matrix->[$i][$j];
-    $ref_matrix->[$i][$j]=$ref_matrix->[$j][$i];
-    $ref_matrix->[$j][$i]=$Bufer;
+    $bufer = $ref_matrix->[$i][$j];
+    $ref_matrix->[$i][$j] = $ref_matrix->[$j][$i];
+    $ref_matrix->[$j][$i] = $bufer;
   }
 }
 
-$Bufer=$rows;
-$rows=$columns;
-$columns=$Bufer;
+$bufer   = $rows;
+$rows    = $columns;
+$columns = $bufer;
 
 if ($rows>$columns){
   for $i (0..$rows){
@@ -95,7 +74,6 @@ if ($rows>$columns){
   }
 
 } elsif ($columns>$rows){
-    $i=$rows;
     while ($#{$ref_matrix}>$rows){
       pop @{$ref_matrix};
     }
